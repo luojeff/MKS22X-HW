82b7cf1ab132fb12ca075dd55907753b5f50760c
Jeffrey Luo <luojeffrey@yahoo.com>
2017-02-11 18:02:49 -0500

    Added other method and organized

--- 02NQueens/QueenBoard.java
+++ 02NQueens/QueenBoard.java
@@ -1,40 +1,62 @@
 import java.util.ArrayList;
 
 /**
- * Generates solutions to the Queens chess problem with n-pieces.
+ * Generates # of solutions to the Queens chess problem with n-pieces. 
  */
 public class QueenBoard {
     private int[][] board;
     private int solutionCount;
 
     private ArrayList<int[]> queenOrder;
-    private int[][] solution;
 
     public QueenBoard(int size) {
 	board = new int[size][size];
+
+	// Keeps track of the order of queens in which they are added to the
+	// board. Useful when the most recently added queen must be removed from
+	// the board
 	queenOrder = new ArrayList<int[]>();
-	solution = new int[size][size];
     }
 
     public void solve() {
-	solveH(0);
-    }	
-	
-    public boolean solveH(int r) {
+	board = new int[board.length][board.length];
+	solveH(0, true);
+    }
+
+    public void countSolutions() {
+	board = new int[board.length][board.length];
+	solutionCount = 0;
+	solveH(0, false);
+    }
+
+    /**
+     * Returns true if the board can be solved with n-queens. If stopAtFirst is
+     * false, the function will instead increment the solutionCount by going
+     * through all the solutions.
+     */
+    public boolean solveH(int r, boolean stopAtFirst) {
 	ArrayList<Integer> openSquares = getOpenSquares(r);
 	if (hasOpenSquares(openSquares)) {
 	    if (r == board.length - 1) {
-		addQueen(r, openSquares.get(0));
-		solutionCount++;
-		return true;
-	    } else {			
+		if (stopAtFirst) {
+		    addQueen(r, openSquares.get(0));
+		    solutionCount++;
+		    return true;
+		} else {
+		    // Don't add a queen to the last row when stopAtFirst is
+		    // false; this newly added queen will be removed but the
+		    // (n-1)th queen will still be in the second to last row
+		    solutionCount++;
+		    return false;
+		}
+	    } else {
 		for (int c : openSquares) {
 		    addQueen(r, c);
-		    if (solveH(r + 1)) {
+		    if (solveH(r + 1, stopAtFirst)) {
 			return true;
 		    } else {
 			int lastQueenRow = queenOrder.get(queenOrder.size() - 1)[0];
-			int lastQueenCol = queenOrder.get(queenOrder.size() - 1)[1];						
+			int lastQueenCol = queenOrder.get(queenOrder.size() - 1)[1];
 			removeQueen(lastQueenRow, lastQueenCol);
 		    }
 		}
@@ -45,14 +67,17 @@ public class QueenBoard {
 	}
     }
 
-    public int getSolutionCount() {
+    public int getCount() {
 	return solutionCount;
     }
 
-    // Adds a queen to the board, including threatened spots
+    /*
+     * Adds a queen to the board (-1) and increments spots which it attacks by 1
+     */
     public void addQueen(int r, int c) {
 	board[r][c] = -1;
 	queenOrder.add(new int[] { r, c });
+		
 	for (int i = 0; i < board.length; i++) {
 	    if (board[i][c] != -1)
 		board[i][c]++;
@@ -92,6 +117,14 @@ public class QueenBoard {
 	}
     }
 
+    /*
+     * Removes a queen from the board, decrementing spots that were attacked by
+     * 1. NOTE: because the recursive algorithm assumes that you can't place
+     * queens in spots that are attacked by other queen(s), removing the queen
+     * replaces the spot with a zero(0), even if it happens to be attacked by
+     * another queen. In the logic of the solveH function, this should still
+     * work.
+     */
     public void removeQueen(int r, int c) {
 	if (board[r][c] == -1) {
 	    for (int i = 0; i < board.length; i++) {
@@ -103,11 +136,6 @@ public class QueenBoard {
 		}
 	    }
 	}
-
-	/*
-	 * int min = Math.min(r, c); int max = Math.max(r, c); int[] topLeft = {
-	 * r - min, c - min }; int[] botLeft = { r + max, c - max };
-	 */
 
 	int[] topLeft = new int[2];
 	int[] botLeft = new int[2];
@@ -142,19 +170,32 @@ public class QueenBoard {
 
 	queenOrder.remove(queenOrder.size() - 1);
 	board[r][c] = 0;
-	// NOTE: REMOVE FUNCTION DOESN'T REPLACE OLD-QUEEN SQUARE
-	// WITH THE PROPER SQUARE DENOTING THE NUMBER OF QUEENS
-	// THREATENING THE SPOT. THIS IS UNDER THE ASSUMPTION
-	// THAT NO QUEENS WILL BE PUT IN A SPOT WHERE THEY ARE
-	// DIRECTLY ATTACKING EACH OTHER
     }
 
+	
     // Displays array in 2-d fashion
+    /* 
+       public String toString() {
+       String retArray = "";
+       for (int[] row : board) {
+       for (int col : row) {
+       retArray += col + " ";
+       }
+       retArray += "\n";
+       }
+       return retArray;
+       }
+    */
+	
     public String toString() {
 	String retArray = "";
-	for (int[] row : board) {
-	    for (int col : row) {
-		retArray += col + " ";
+	for(int[] row : board){
+	    for(int col: row){
+		if(col >= 0){
+		    retArray += "_ ";
+		} else {
+		    retArray += "Q ";
+		}
 	    }
 	    retArray += "\n";
 	}
