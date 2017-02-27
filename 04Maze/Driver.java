97a43f5c2cf99366248b96ebdcacf0892a29af6e
Jeffrey Luo <luojeffrey@yahoo.com>
2017-02-27 15:36:50 -0500

    Added stuff

---
+++ 04Maze/Driver.java
@@ -0,0 +1,14 @@
+public class Driver{
+
+    public static void main(String[]args){
+        Maze f;
+        f = new Maze("data1.dat");
+        
+        f.setAnimate(false);
+        f.solve();
+
+        System.out.println(f);
+    }
+}
+
+
