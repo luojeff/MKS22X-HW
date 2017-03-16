58117f63506e97bae3641e4481a19ab901861cd8
Jeffrey Luo <luojeffrey@yahoo.com>
2017-03-16 19:22:53 -0400

    Working partition and quickselect

--- 07Quick/Quick.java
+++ 07Quick/Quick.java
@@ -1,29 +1,29 @@
 public class Quick {
     public static int part(int[] data, int start, int end){
+	/* goes only through subsection */
 	int[] vals = new int[end-start];
 		
 	for(int i=start; i<end; i++){
-	    vals[i-start] = data[i];
+	    vals[i-start] = data[i];	    
 	}
 
-	int pivot = vals[(int)(Math.random() * vals.length)];	
-       
-	int scount = start;
-	int ecount = end-1;
+	int pivotIndex = (int)(Math.random() * vals.length);
+	int pivot = vals[pivotIndex];
+	end--;
 	
 	for(int i=0; i<vals.length; i++){
-	    if(vals[i] <= pivot){
-		data[scount] = vals[i];
-		scount++;
-	    } else if (vals[i] > pivot){
-		data[ecount] = vals[i];
-		ecount--;
+	    if(i != pivotIndex){
+		if(vals[i] <= pivot){
+		    data[start++] = vals[i];	        
+		} else if (vals[i] > pivot){
+		    data[end--] = vals[i];
+		}
 	    }
 	}
-	
-	data[scount] = pivot;
-	
-        return scount;
+
+	data[start] = pivot;
+		
+        return start;
     }
 
     public static int quickselect(int[] data, int k){
@@ -31,32 +31,29 @@ public class Quick {
 	int end = data.length;
         
 	int piv = part(data, start, end);
-	System.out.println("Current piv: " + piv);
 
-	k++;
         while (piv != k){
 	    if(piv < k){
 		start = piv;
 	    } else if (piv > k){
-		end = piv;
+		end = piv;		
 	    }
+
 	    piv = part(data, start, end);
-	    System.out.println("Current piv: " + piv);
 	}
 
 	return data[piv];
     }
 
     public static void main(String[] args){
-        int[] a = new int[]{9, 3, 11, 4, 20, 1, 7, 4};
+        int[] a = new int[]{9, 4, 3, 7, 1, 8, 6, 2, 5, 0};
 
-	
-	System.out.println("Element: " + quickselect(a, 0));
+	/* testing */
+        System.out.println("\nElement: " + quickselect(a, 0));
 
-	/*
+	System.out.print("Partitioned Array:\n");
 	for(int i : a){
-	    System.out.println(i);
+	    System.out.print(i + " ");
 	}
-	*/
     }
 }
