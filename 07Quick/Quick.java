25284e75f09b378376e541857fc11f05b641a29b
luojeff <luojeffrey@yahoo.com>
2017-03-18 19:51:32 -0400

    Finished quickSort

--- 07Quick/Quick.java
+++ 07Quick/Quick.java
@@ -1,59 +1,90 @@
+import java.lang.StringBuilder;
+import java.io.*;
+
 public class Quick {
     public static int part(int[] data, int start, int end){
-	/* goes only through subsection */
-	int[] vals = new int[end-start];
-		
-	for(int i=start; i<end; i++){
-	    vals[i-start] = data[i];	    
-	}
-
-	int pivotIndex = (int)(Math.random() * vals.length);
-	int pivot = vals[pivotIndex];
-	end--;
 	
-	for(int i=0; i<vals.length; i++){
-	    if(i != pivotIndex){
-		if(vals[i] <= pivot){
-		    data[start++] = vals[i];	        
-		} else if (vals[i] > pivot){
-		    data[end--] = vals[i];
-		}
+	/* Chooses pivot */
+	int pivotIndex = (int)(Math.random() * (end - start + 1)) + start;
+	int pivot = data[pivotIndex];
+
+	/* Goes through "unprocessed" portion of the array */
+	int curr = start;
+
+	/* Makes first element equal to pivot */
+	swap(data, start, pivotIndex);
+	
+	while(curr <= end){
+	    if(data[curr] > pivot){	        
+		swap(data, curr, end--);	        
+	    } else if (data[curr] < pivot){
+		swap(data, curr++, start++);	        
+	    } else {
+		curr++;
 	    }
 	}
-
-	data[start] = pivot;
-		
-        return start;
+	
+        return curr-1;
     }
 
+    /* 
+       Swaps elements of array at indices a and b 
+       respectively
+    */
+    private static void swap(int[] data, int a, int b){
+	int bVal = data[b];
+	data[b] = data[a];
+	data[a] = bVal;
+    }
+
+    /*
+      Finds k-th smallest element by partitioning until the 
+      pivot generated is the k-th index, in which it returns
+      the value at that certain pivot point
+    */
     public static int quickselect(int[] data, int k){
 	int start = 0;
-	int end = data.length;
-        
+	int end = data.length-1;
+
+	/* initial partition */
 	int piv = part(data, start, end);
 
         while (piv != k){
 	    if(piv < k){
 		start = piv;
 	    } else if (piv > k){
-		end = piv;		
+		end = piv;
 	    }
 
-	    piv = part(data, start, end);
+	    piv = part(data, start, end);	    
 	}
-
+	
 	return data[piv];
     }
 
-    public static void main(String[] args){
-        int[] a = new int[]{9, 4, 3, 7, 1, 8, 6, 2, 5, 0};
+    /*
+      Sorts by continuously partitioning partitions (recursively)
+      until each element is separated by a partition; by this point
+      the array is sorted
+    */
+    public static void quickSort(int[] data){
+	quickSortH(data, 0, data.length-1);
+    }
+    
+    private static void quickSortH(int[] data, int start, int end){
+	int indPart = part(data, start, end);
 
-	/* testing */
-        System.out.println("\nElement: " + quickselect(a, 0));
+	/* Recursive call on two subsequent partitions */
+	if(end > indPart){
+	    quickSortH(data, indPart+1, end);	    
+	}
 
-	System.out.print("Partitioned Array:\n");
-	for(int i : a){
-	    System.out.print(i + " ");
+	if(indPart > start){
+	    quickSortH(data, start, indPart-1);
 	}
     }
+
+
+    public static void main(String[] args){
+    }    
 }
