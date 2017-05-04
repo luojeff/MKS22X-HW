691bb632f50cf448c00431dfdfb0bedd381f49cb
luojeff <luojeffrey@yahoo.com>
2017-05-03 23:29:10 -0400

    Added essential methods to MyHeap

--- 13Heap/MyHeap.java
+++ 13Heap/MyHeap.java
@@ -1,41 +1,64 @@
 public class MyHeap {
-    String[] arr;
-    int size;
+    private String[] arr;
+    private int size;
+    private int curr;
 
-    int multiplier = 1;
+    private int multiplier = 1;
 
     public MyHeap(){
-	arr = new String[1];
-	size = arr.length; 
+	arr = new String[4];
+	size = arr.length;
+	curr = 0;
     }
 
     public MyHeap(boolean min){
-	arr = new String[1];
-	size = arr.length; 
+	this();
 	if(min){
 	    multiplier = -1;
 	}
     }
 
     public void add(String s){
+	if(++curr < size){
+	    arr[curr] = s;
+	} else {	
+	    resize();
+	    arr[curr] = s;
+	}
     }
 
     public String remove(){
-	return "";
+	String ret = arr[curr];
+	arr[curr--] = null;
+	
+	return ret;
     }
 
     public String peek(){
-	return "";
+	return arr[curr];
     }
 
     private void resize(){
-	size*=2;
-	arr = new String[size];
-    }
+	size *= 2;
+	String[] newArr = new String[size];
+
+	for(int i=0; i<arr.length; i++){
+	    newArr[i] = arr[i];
+	}
+
+	arr = newArr;
+    }    
 
     private void pushUp(){}
     private void pushDown(){}
 
-    public static void main(String[] args){	
+    public static void main(String[] args){
+	MyHeap h = new MyHeap();
+	h.add("hello");
+	h.add("world!");
+
+	System.out.println(h.peek());
+	System.out.println(h.remove());
+	System.out.println(h.peek());
     }
 }
