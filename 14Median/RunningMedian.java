a6acf551c7e7e683535878a9a4b4f282cf96c447
Jeffrey Luo <luojeffrey@yahoo.com>
2017-05-08 23:14:13 -0400

    Completed methods

--- 14Median/RunningMedian.java
+++ 14Median/RunningMedian.java
@@ -1,11 +1,11 @@
 public class RunningMedian {
-    private MyHeap small;
-    private MyHeap large;
-    private int smallSize, largeSize, median;
+    private MyHeap small, large;
+    private int smallSize, largeSize;
+    private double median;
     
     public RunningMedian(){
 	small = new MyHeap(true); // max heap
-	large = new MyHeap(false); // min heap
+	large = new MyHeap(false); // min heap       
 	smallSize = 0;
 	largeSize = 0;
     };
@@ -13,12 +13,64 @@ public class RunningMedian {
     public void add(int i){
 	if(i < median){
 	    small.add(i);
+	    smallSize++;
 	} else {
-	    large.add(i);
+ 	    large.add(i);
+	    largeSize++;
+	}
+
+	balanceHeaps();
+	calculateMedian();	
+    }
+
+    private void balanceHeaps(){
+	if(smallSize < largeSize){
+	    small.add(large.remove());
+	    smallSize++;
+	    largeSize--;
+	} else if (largeSize > smallSize){
+	    large.add(small.remove());
+	    largeSize++;
+	    smallSize--;
+	}
+    }
+
+    private void calculateMedian(){
+	if(smallSize == largeSize){
+	    median = (double)(small.peek() + large.peek())/2;
+	} else if (Math.abs(smallSize - largeSize) == 1){
+	    if(smallSize < largeSize){
+		median = (double)large.peek();
+	    } else {
+		median = (double)small.peek();
+	    }
 	}
     }
 
     public double getMedian(){
-	return 1.0;
+	return median;
+    }
+
+    // For debugging
+    public String toString(){
+	return "small: " + small + "\nlarge: " + large;
+    }
+
+    public static void main(String[] args) {
+	RunningMedian rm = new RunningMedian();
+
+	rm.add(10);
+	rm.add(34);
+	rm.add(5);
+	rm.add(20);
+	rm.add(-10);
+	rm.add(1);
+	rm.add(19);
+	rm.add(0);
+	rm.add(35);
+	rm.add(-4);
+	rm.add(12);
+
+	System.out.println(rm.getMedian());	
     }
 }
