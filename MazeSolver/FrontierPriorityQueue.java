dbe8d405cc08f8cc6ef2f2b96f3797b5b9e4c67d
Jeffrey Luo <luojeffrey@yahoo.com>
2017-05-16 15:24:10 -0400

    Added template classes, working on methods

--- MazeSolver/FrontierPriorityQueue.java
+++ MazeSolver/FrontierPriorityQueue.java
@@ -1,3 +1,20 @@
 public class FrontierPriorityQueue {
+    MyHeap myHeap;
+
+    public FrontierPriorityQueue(){
+	myHeap = new MyHeap(true);
+    }
+
+    public void add(Location location){
+	myHeap.add(location);
+    }
+
+    public Location next(){
+	try {
+	    return myHeap.remove();
+	} else {
+	    return null;
+	}
+    }
 
 }
--- MazeSolver/Location.java
+++ MazeSolver/Location.java
@@ -4,16 +4,16 @@ public class Location implements Comparable<Location>{
     private Location previous;
     private boolean aStar;
 
-    public Location(int r, int c, Location previous, int toStart, int toGoal, boolean aStar){
-	row = r;
-	col = c;
-	previous = p;
+    public Location(int row, int col, Location prev, int toStart, int toGoal, boolean aStar){
+	this.row = row;
+	this.col = col;
+	previous = prev;
 	distToStart = toStart;
 	distToGoal = toGoal;
 	this.aStar = aStar;
     }
 
     public int compareTo(Location other){
-	return -1;
+	return (distToStart + distToGoal) - other.distToStart - other.distToGoal;
     }
 }
