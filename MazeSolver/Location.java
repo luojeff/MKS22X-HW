231bd132ad1fc07bfcbcdd1e82989c119771ab41
Jeffrey Luo <luojeffrey@yahoo.com>
2017-05-18 15:30:08 -0400

    Added Frontier initializations, reorganized Location class

--- MazeSolver/Location.java
+++ MazeSolver/Location.java
@@ -1,16 +1,12 @@
 public class Location implements Comparable<Location>{
     private int row, col;
     private int distToStart, distToGoal;
-    private Location previous;
-    private boolean aStar;
 
-    public Location(int row, int col, Location prev, int toStart, int toGoal, boolean aStar){
+    public Location(int row, int col, int toStart, int toGoal){
 	this.row = row;
 	this.col = col;
-	previous = prev;
 	distToStart = toStart;
 	distToGoal = toGoal;
-	this.aStar = aStar;
     }
 
     public int compareTo(Location other){
--- MazeSolver/MazeSolver.java
+++ MazeSolver/MazeSolver.java
@@ -1,15 +1,36 @@
 public class MazeSolver {
-    Maze maze;
+    public Maze maze;
+    public Frontier frontier;
+    public boolean animate;
 
-    public MazeSolver(String s){
-	maze = new Maze(s);
+    public MazeSolver(String filename){
+	this(filename, false);
+    }
+   
+    public MazeSolver(String filename, boolean animate){
+	this.animate = animate;
+        maze = new Maze(filename);
     }
 
-    public void solve(){
-	
+    public void solve(){	
     }
 
-    public void solve(int i){
+    public void solve(int style){
+	switch(style){
+	case 0:
+	    frontier = new StackFrontier();
+	case 1:
+	    frontier = new QueueFrontier();
+	case 2:
+	    frontier = new FrontierPriorityQueue();
+	case 3:
+	    frontier = new FrontierPriorityQueue();
+        default:
+	}
+
+	frontier.add(maze.getStart());
+
+	// PROCESS FRONTIER HERE
     }
 
     public static void main(String[] args){
